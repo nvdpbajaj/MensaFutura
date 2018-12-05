3567cbf53de67d0529e3ada29590239ffb7281b7
nvdpbajaj <45624260+nvdpbajaj@users.noreply.github.com>
2018-12-05 16:03:21 +0530

    Mensa Futura

---
+++ js/hovers.js
@@ -0,0 +1,29 @@
+/**
+
+ */
+(function() {
+	
+	function init() {
+		var speed = 250,
+			easing = mina.easeinout;
+
+		[].slice.call ( document.querySelectorAll( '.tdoctor' ) ).forEach( function( el ) {
+			var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+				pathConfig = {
+					from : path.attr( 'd' ),
+					to : el.getAttribute( 'data-path-to' )
+				};
+
+			el.addEventListener( 'mouseenter', function() {
+				path.animate( { 'path' : pathConfig.to }, speed, easing );
+			} );
+
+			el.addEventListener( 'mouseleave', function() {
+				path.animate( { 'path' : pathConfig.from }, speed, easing );
+			} );
+		} );
+	}
+
+	init();
+
+})();
